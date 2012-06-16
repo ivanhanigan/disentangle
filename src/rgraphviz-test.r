711e9422baafd4d48f8f52a24f54633d49d58fdd
Ivan Hanigan <ivan.hanigan@gmail.com>
2012-06-16 10:12:35 +1000

    rgraphviz test

---
+++ src/rgraphviz-test.r
@@ -0,0 +1,7 @@
+
+# http://www.bioconductor.org/packages/2.9/bioc/html/Rgraphviz.html
+  source("http://bioconductor.org/biocLite.R")
+  biocLite("Rgraphviz")
+# update all?
+require(Rgraphviz)
+example(layoutGraph)
