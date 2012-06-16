cbb38d1c21da6d2bfddaf1305a8b2cdf079c8a94
Ivan <ivan@RADHA-HP.(none)>
2012-06-17 00:12:00 +1000

    edges test

--- edges-test.R
+++ edges-test.R
@@ -1,5 +1,27 @@
 require(Rgraphviz)
 require(biocGraph)
+newnode<-function(dsc, i, o=NA, append=F, notes=F, code=NA, ttype=NA){
+#   dsc='Clean Weather Data',
+#  ttype='data',
+#  i='BOM',
+#  o='Weather Data',
+#   append=F,
+#   notes='Error Checking',
+#   code
+  
+  if(!exists('rEG')) {
+    rEG <- new("graphNEL", nodes=c("outcome", "population", "exposure",'analyte'), edgemode="directed")
+  }
+  rEG <- addNode(node=dsc,object=rEG) 
+  rEG <- addNode(node=i,object=rEG) 
+  rEG <- addEdge(i, dsc, rEG, 1)
+  if(!is.na(o)){
+    rEG <- addEdge(from=dsc, to=o, rEG, 1)  
+  }
+  return(rEG)
+}
+
+
 
 rEG <- new("graphNEL", nodes=c("outcome", "population", "exposure",'analyte'), edgemode="directed")
 rEG <- addEdge("outcome", "analyte", rEG, 1)
@@ -15,16 +37,37 @@ rEG <- addEdge("analyte", 'eda', rEG, 1)
 rEG <- addNode(node='model1',object=rEG) 
 rEG <- addEdge('eda', 'model1', rEG, 1)
 
-newnode<-function(nodeName, input){
-  if(!exists('rEG')) {
-    rEG <- new("graphNEL", nodes=c("outcome", "population", "exposure",'analyte'), edgemode="directed")
-  }
-rEG <- addNode(node=nodeName,object=rEG) 
-rEG <- addEdge(input, nodeName, rEG, 1)
-return(rEG)
-}
 
-rEG <- newnode('modelChecking', 'model1')
-rEG <- newnode('report', 'model1')
-dev.off()
+rEG <- newnode(dsc='modelChecking', i='model1')
+rEG <- newnode(dsc='report', i='model1')
+
+
+
+rEG<-newnode(dsc='Weather Data',ttype='data',i='BOM',o='exposure',
+        append=F,
+        notes='Error Checking',
+        code=NA)
+
+rEG<-newnode(dsc='EWE criteria',i='algorithms', o='exposure',
+        notes='',
+        code=NA)
 plot(rEG)
+dev.off()
+defAttrs <- getDefaultAttrs()
+defAttrs
+
+width = 1600
+height = 2500
+png('edges-test.png', res=150, width=width, height=height)
+par(mai=rep(0,4))
+plot(rEG,attrs=list(node=list(label="foo", fillcolor="grey",shape="ellipse", fixedsize=FALSE),
+                    edge=list(color="black")))
+dev.off()
+
+width = height = 512
+png('edges-test.png', width=width, height=height)
+par(mai=rep(0,4))
+plot(rEG,attrs=list(node=list(label="foo", fillcolor="lightgreen"),
+                     edge=list(color="cyan"),
+                     graph=list(rankdir="LR")))
+dev.off()
