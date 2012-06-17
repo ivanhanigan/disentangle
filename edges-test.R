e51c768d80608c865c39d9be46791b4896e32d4c
Ivan <ivan@RADHA-HP.(none)>
2012-06-17 22:58:31 +1000

    newnode 2, with hardcoded delphe

--- edges-test.R
+++ edges-test.R
@@ -1,6 +1,4 @@
-require(Rgraphviz)
-require(biocGraph)
-newnode<-function(dsc, i, o=NA, append=F, notes=F, code=NA, ttype=NA){
+newnode<-function(dsc, i, o=NA, append=T, notes=F, code=NA, ttype=NA){
 #   dsc='Clean Weather Data',
 #  ttype='data',
 #  i='BOM',
@@ -8,12 +6,21 @@ newnode<-function(dsc, i, o=NA, append=F, notes=F, code=NA, ttype=NA){
 #   append=F,
 #   notes='Error Checking',
 #   code
+  require(Rgraphviz)
+  require(biocGraph)
   
-  if(!exists('rEG')) {
-    rEG <- new("graphNEL", nodes=c("outcome", "population", "exposure",'analyte'), edgemode="directed")
+#  if(!exists('rEG')) {
+if(append==F) {
+    
+  rEG <- new("graphNEL", nodes=c("outcome", "population", "exposure",'analyte'),
+               edgemode="directed")
+    rEG <- addEdge("outcome", "analyte", rEG, 1)
+    rEG <- addEdge("population", "analyte", rEG, 1)
+    rEG <- addEdge("exposure", "analyte", rEG, 1)
+    
   }
   rEG <- addNode(node=dsc,object=rEG) 
-  rEG <- addNode(node=i,object=rEG) 
+#  rEG <- addNode(node=i,object=rEG) 
   rEG <- addEdge(i, dsc, rEG, 1)
   if(!is.na(o)){
     rEG <- addEdge(from=dsc, to=o, rEG, 1)  
@@ -23,55 +30,52 @@ newnode<-function(dsc, i, o=NA, append=F, notes=F, code=NA, ttype=NA){
 
 
 
-rEG <- new("graphNEL", nodes=c("outcome", "population", "exposure",'analyte'), edgemode="directed")
-rEG <- addEdge("outcome", "analyte", rEG, 1)
-rEG <- addEdge("population", "analyte", rEG, 1)
-rEG <- addEdge("exposure", "analyte", rEG, 1)
-plot(rEG)
-rEG <- addNode(node='qc',object=rEG) #,edges=list(c('analyte')))
-rEG <- addEdge("analyte", 'qc', rEG, 1)
+# rEG <- new("graphNEL", nodes=c("outcome", "population", "exposure",'analyte'), edgemode="directed")
+# plot(rEG)
+# rEG <- addNode(node='qc',object=rEG) #,edges=list(c('analyte')))
+# rEG <- addEdge("analyte", 'qc', rEG, 1)
+# 
+# rEG <- addNode(node='eda',object=rEG) 
+# rEG <- addEdge("analyte", 'eda', rEG, 1)
+# 
+# rEG <- addNode(node='model1',object=rEG) 
+# rEG <- addEdge('eda', 'model1', rEG, 1)
 
-rEG <- addNode(node='eda',object=rEG) 
-rEG <- addEdge("analyte", 'eda', rEG, 1)
-
-rEG <- addNode(node='model1',object=rEG) 
-rEG <- addEdge('eda', 'model1', rEG, 1)
-
+rEG <- newnode(dsc='model1', i='analyte', append=F)
 
 rEG <- newnode(dsc='modelChecking', i='model1')
-rEG <- newnode(dsc='report', i='model1')
-
-
-
-rEG<-newnode(dsc='Weather Data',ttype='data',i='BOM',o='exposure',
-        append=F,
-        notes='Error Checking',
-        code=NA)
-
-rEG<-newnode(dsc='EWE criteria',i='algorithms', o='exposure',
-        notes='',
-        code=NA)
-
-rEG<-newnode(dsc='Population Data',ttype='data',o='population', i =c('ABS', 'Consistent Spatial Framework'),
-        notes='',
-        code=NA)
-rEG <- newnode(dsc='Select Mortality Data',ttype='data', i = c('Ethics Approval', 'Specific Causes of Death'), 
-               o= 'outcome',
-               notes='',
-               code=NA)
-rEG <- addEdge('Consistent Spatial Framework', 'Select Mortality Data', rEG, 1)
-
-plot(rEG)
-dev.off()
-defAttrs <- getDefaultAttrs()
-defAttrs
-
-width = 2500
-height = 2500
-png('edges-test.png', res=500, width=width, height=height)
-par(mai=rep(0,4))
-plot(rEG,attrs=list(node=list(label="foo", fillcolor="grey",shape="ellipse", fixedsize=FALSE),
-                    edge=list(color="black")))
+# rEG <- newnode(dsc='report', i='model1')
+# rEG<-newnode(dsc='Weather Data',ttype='data',i='BOM',o='exposure',
+#   append=F,
+#   notes='Error Checking',
+#   code=NA)
+# rEG<-newnode(dsc='EWE criteria',i='algorithms', o='exposure',
+#   notes='',
+#   code=NA)
+# rEG<-newnode(dsc='Population Data',ttype='data',o='population', 
+#   i =c('ABS', 'Consistent Spatial Framework'),
+#   notes='',
+#   code=NA)
+# rEG <- newnode(dsc='Select Mortality Data',ttype='data', 
+#   i = c('Ethics Approval', 'Specific Causes of Death'), 
+#   o= 'outcome',
+#   notes='',
+#   code=NA)
+# rEG <- addEdge('Consistent Spatial Framework', 'Select Mortality Data', rEG, 1)
+# 
+# plot(rEG)
+# dev.off()
+# defAttrs <- getDefaultAttrs()
+# defAttrs
+# 
+# width = 2500
+# height = 2500
+# png('edges-test.png', res=500, width=width, height=height)
+# par(mai=rep(0,4))
+plot(rEG,attrs=list(node=list(label="foo", 
+  fillcolor="grey",shape="ellipse", 
+  fixedsize=FALSE),
+  edge=list(color="black")))
 dev.off()
 
 # width = height = 512
