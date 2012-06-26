83ba817757337ad0ee023bad0a59ab295756de85
ivan_hanigan <ivan_hanigan@i-000015bf.(none)>
2012-06-26 17:08:16 +1000

    newnodes for example

--- src/newnode_test/newnode-test.r
+++ src/newnode_test/newnode-test.r
@@ -6,6 +6,17 @@ rEG <- newnode(dsc = 'clean', i = 'abs', o = 'outcome')
 rEG <- newnode(dsc = 'outcome', i = 'Clean Weather Data', o = 'join')
 rEG <- newnode(dsc = 'epxosure', i ='flu', o = 'join')
 rEG <- newnode(dsc = 'other', i =c('aihw','state','abs'), o = 'join')
+rEG <- newnode(dsc = 'hospitalFlu', i ='state', o ='flu')
+rEG <- newnode(dsc = 'DeathsFlu', i ='abs', o ='flu')
+rEG <- newnode(dsc = 'pops', i ='abs', o ='join')
+rEG <- newnode(dsc = 'seifa', i ='abs', o ='epxosure')
+
+rEG <- newnode(dsc = 'radha', i ='newinput', o ='DeathsFlu')
+rEG <- newnode(dsc = 'radha', i ='newinput2', o ='outcome')
+rEG <- newnode(dsc = 'radha', i ='newinput3', o ='newinput2')
+rEG <- addEdge(from='newinput2',to='newinput3',graph=rEG,weights=1)
+rEG <- addEdge(from='radha',to='pops',graph=rEG,weights=1)
+
 
 dev.off()
 plot(rEG,attrs=list(node=list(label="foo", fillcolor="grey",shape="ellipse", fixedsize=FALSE), edge=list(color="black")))
