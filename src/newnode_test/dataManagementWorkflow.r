a73ec2787de1e3091896805d168eb8263be62961
Ivan Hanigan <ivan.hanigan@gmail.com>
2012-10-18 23:00:51 +1100

    new graph of data workflow

--- src/newnode_test/dataManagementWorkflow.r
+++ src/newnode_test/dataManagementWorkflow.r
@@ -1,38 +1,59 @@
 #setwd('~/tools/disentangle')
-source('src/newnode.r')
+dir()
+source('../newnode.r')
 nodes <- newnode(name = 'aquire the raw data',
-                 inputs = c('external sources','collected by researcher'),
-                 outputs = c('file server','metadata database','cleaning'), newgraph=T)
+                 inputs = c('external sources',
+                   'collected by researcher'),
+                 outputs =
+                 c('file server','metadata database','cleaning'),
+                 newgraph=T)
 
 nodes <- newnode(name = 'database server',
                  inputs = 'file server',
-                 outputs = c('metadata database'))
+                 outputs = c('metadata database')
+                 )
 
-nodes <- newnode(name = 'search engine web server', inputs ='metadata database',
-                 outputs = NA)
+nodes <- newnode(name = 'search engine web server',
+                 inputs ='metadata database',
+                 outputs = NA
+                 )
 
 
-nodes <- newnode(name = 'calculate new data', inputs = 'database server',
+nodes <- newnode(name = 'calculate new data',
+                 inputs = 'database server',
                  outputs =
                  c('analyse using stats package','database server',
-                 'metadata database', 'technical documentation'))
+                   'metadata database', 'technical documentation')
+                 )
 
 nodes <- newnode(name = 'analyse using stats package',
-                 inputs = c('aquire the raw data','database server','simulation',
-                            'file server'),
+                 inputs = c('aquire the raw data',
+                   'database server','simulation',
+                   'file server'),
                  outputs = c('results', 'metadata database',
-                 'cleaning', 'technical documentation'))
+                   'cleaning', 'technical documentation')
+                 )
+
 # I want to feedback to database
 #nodes <- addEdge(from='analyse using stats package',
 #                 to='database server',graph=nodes,weights=1)
 
-nodes <- newnode(name = 'communicate the results', inputs ='results',
-                 outputs = c('journal publication'))
+nodes <- newnode(name = 'communicate the results',
+                 inputs ='results',
+                 outputs = c('journal publication')
+                 )
+
 nodes <- newnode(name='technical documentation',
-                 inputs = c('aquire the raw data','communicate the results'),
-                 outputs = 'cleaning')
-nodes <- newnode(name = 'archive at end of project', inputs ='journal publication',
-                 outputs = c('repurposed data','file server', 'destroy'))
+                 inputs = c('aquire the raw data',
+                   'communicate the results'),
+                 outputs = 'cleaning'
+                 )
+
+nodes <- newnode(name = 'archive at end of project',
+                 inputs ='journal publication',
+                 outputs = c('repurposed data','file server',
+                   'destroy')
+                 )
 
 
 #plot(nodes,attrs=list(node=list(label="foo", fillcolor="grey",shape="ellipse", fixedsize=FALSE), edge=list(color="black")))
