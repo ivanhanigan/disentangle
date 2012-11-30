de9c95625d08c07c5609089d59a1f4bf53affc0a
Ivan Hanigan <ivan.hanigan@gmail.com>
2012-11-30 23:43:42 +1100

    remove old disentangle things

--- README.md
+++
@@ -1,6 +0,0 @@
-disentanglethings
-=================
-
-Toolbox for disentangling things
-
-Ivan Hanigan 2012-04-26
--- main.r
+++
@@ -1,9 +0,0 @@
-
-require(ProjectTemplate)
-# create.project('disentangle2')
-# copy the PT dirs up one level
-load.project()
-# add a function to the lib dir and then
-stub.tests()
-# work on the functions and run
-test.project()
