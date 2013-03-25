9fca6be8afb1606c48afd0ad5381d932277dc7d7
ivanhanigan <ivanhanigan@gmail.com>
2013-03-25 12:58:10 +1100

    add mods to estat

--- R/estat.r
+++ R/estat.r
@@ -7,25 +7,33 @@
 # a function to get Akaike's and Schwarz's Bayesian information criteria.
   # named after stata function
   estat <- function(modGLM,modName,createCsv=F){
-    if(!exists('aic_table'))
-      {
-        aic_table <- matrix(ncol=5,nrow=0)
-      }
+      estats <- c(modName,
+                    length(coef(modGLM)),
+                    AIC(modGLM),
+                    AIC(modGLM, k = log(nrow(analyte))),
+                    ((modGLM$null.deviance - modGLM$deviance)/
+                      modGLM$null.deviance)*100
+        )
+        estats <- as.data.frame(t(estats))
+        names(estats) <- c('model','param','aic','bic','percentChDev')
+        estats$model       <-as.character(estats$model)
+        estats$param       <-as.numeric(as.character(estats$param       ))
+        estats$aic         <-as.numeric(as.character(estats$aic         ))
+        estats$bic         <-as.numeric(as.character(estats$bic         ))
+        estats$percentChDev<-as.numeric(as.character(estats$percentChDev))
+     if(!exists('aic_table'))
+        {
+          aic_table <- estats
+        } else {
+          aic_table <- rbind(aic_table,estats)
+        }
+     aic_table <- aic_table[order(aic_table$bic),]
+     # write to csv
+     # if(createCsv==T){
+     #   write.table(as.data.frame(t(estats)), 'aic_table.csv', sep=',', row.names=F, append=F, col.names=F)
+     # } else {
+     #   write.table(as.data.frame(t(estats)), 'aic_table.csv', sep=',', row.names=F, append=T, col.names=F)
+     # }
 
-    estats <- c(modName,
-                length(coef(modGLM)),
-                AIC(modGLM),
-                AIC(modGLM, k = log(nrow(analyte))),
-                ((modGLM$null.deviance - modGLM$deviance)/ modGLM$null.deviance)*100
-    )
-
-    aic_table <- rbind(aic_table,estats)
-
-    # write to csv
-    # if(createCsv==T){
-    #   write.table(as.data.frame(t(estats)), 'aic_table.csv', sep=',', row.names=F, append=F, col.names=F)
-    # } else {
-    #   write.table(as.data.frame(t(estats)), 'aic_table.csv', sep=',', row.names=F, append=T, col.names=F)
-    # }
-    return(aic_table)
+     return(aic_table)
   }
