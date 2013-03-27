6a5d03f2e5139ba54c887a468b2aaf77b78c46aa
ivanhanigan <ivanhanigan@gmail.com>
2013-03-27 11:13:08 +1100

    mod estat include modname and formula

--- R/estat.r
+++ R/estat.r
@@ -7,13 +7,13 @@
 # name:estat
 # a function to get Akaike's and Schwarz's Bayesian information criteria.
   # named after stata function
-  estat <- function(modGLM, modName, read_previous_aictable = FALSE){
+  estat <- function(modGLM, modName, form = NA, order_by_aic = TRUE, read_previous_aictable = FALSE){
          if(read_previous_aictable & file.exists("aictable.csv"))
            {
              aictable <- read.csv('aictable.csv')
            }
 
-      estats <- c(modName,
+      estats <- c(modName, form,
                   length(coef(modGLM)),
                   (-2 * logLik(modGLM)[1] + 2 * length(modGLM$coeff)),
                   (-2 * logLik(modGLM)[1] + log(nrow(modGLM$data)) * length(modGLM$coeff)),
@@ -21,8 +21,9 @@
                     modGLM$null.deviance)*100)
         )
         estats <- as.data.frame(t(estats))
-        names(estats) <- c('model','param','aic','bic','percentChDev')
+        names(estats) <- c('model','formula','param','aic','bic','percentChDev')
         estats$model       <-as.character(estats$model)
+        estats$formula       <-as.character(estats$formula)
         estats$param       <-as.numeric(as.character(estats$param       ))
         estats$aic         <-as.numeric(as.character(estats$aic         ))
         estats$bic         <-as.numeric(as.character(estats$bic         ))
@@ -33,6 +34,11 @@
         } else {
           aictable <- rbind(aictable,estats)
         }
-     aictable <- aictable[order(aictable$bic),]
+     if(order_by_aic)
+       {
+        aictable <- aictable[order(aictable$aic),]
+       } else {
+        aictable <- aictable[order(aictable$bic),]
+       }
      return(aictable)
   }
