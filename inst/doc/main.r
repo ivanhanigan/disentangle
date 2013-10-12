a0b5b742d2f59ecf23565ed148003872869178e3
ivanhanigan <ivan.hanigan@gmail.com>
2013-10-12 22:12:21 +1100

    add the ghpages stuff

---
+++ inst/doc/main.r
@@ -0,0 +1,8 @@
+
+################################################################
+# devtools is recommended
+require(devtools)
+install_github("disentangle", "ivanhanigan")
+require(disentangle)
+
+source("tests/test-tree.chisq.r")
