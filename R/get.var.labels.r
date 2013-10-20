a5127a47691ea69246f292dcaabf3ba086110a16
ivanhanigan <ivanhanigan@gmail.com>
2013-10-20 22:07:58 +1100

    add get var labels

---
+++ R/get.var.labels.r
@@ -0,0 +1,16 @@
+
+################################################################
+# name:get.var.labels
+# from http://stackoverflow.com/a/10261534
+# this creates the $variable.labels attribute
+get.var.labels = function(data) {
+  if(!require(Hmisc)) install.packages('Hmisc', dependencies = TRUE); require(Hmisc)
+  a = do.call(llist, data)
+  tempout = vector("list", length(a))
+
+  for (i in 1:length(a)) {
+    tempout[[i]] = label(a[[i]])
+  }
+  b = unlist(tempout)
+  structure(c(b), .Names = names(data))
+}
