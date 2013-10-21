9b2cdf51efcee214868449bb20bb24d7619a8543
ivanhanigan <ivanhanigan@gmail.com>
2013-10-21 16:35:25 +1100

    Latest build.

--- R/spss_variable_labels_read.r
+++ R/spss_variable_labels_read.r
@@ -1,7 +1,7 @@
 
 ################################################################
 # name:R-spss-variable-labels-read
-spss_variable_labels_read  <- function(x, filter, case_sensitive = FALSE)
+spss_variable_labels_read  <- function(x, filter, case_sensitive = FALSE, return_df = FALSE)
 {
   if(case_sensitive)
     {
@@ -10,8 +10,18 @@ spss_variable_labels_read  <- function(x, filter, case_sensitive = FALSE)
       col_index  <- grep(tolower(filter), tolower(attributes(x)$variable.labels))      
     }
   names_returned <- attributes(x)$variable.labels[col_index]
-  names_returned <- paste(names_returned, sep = "", collapse = "', '")
-  cat(sprintf("returning the columns '%s'", names_returned))
-  data_out <- x[,col_index]
-  return(data_out)
+  col_names  <- names(names_returned)
+  col_refs  <-  as.data.frame(cbind(col_names, names_returned))
+  col_refs[,1]  <-  as.character(col_refs[,1])
+  col_refs[,2]  <-  as.character(col_refs[,2])
+  row.names(col_refs)  <- NULL
+  if(return_df)
+    {
+      names_returned <- paste(names_returned, sep = "", collapse = "', '")
+      cat(sprintf("returning the columns '%s'", names_returned))
+      data_out <- x[,col_index]
+      return(data_out)
+    } else {
+      return(col_refs)
+    }   
 }
