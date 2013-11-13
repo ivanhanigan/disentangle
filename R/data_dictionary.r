f8fb984c1c44d4f4110ba0bd08e7870d7a9afb19
ivanhanigan <ivan.hanigan@gmail.com>
2013-11-13 21:45:59 +1100

    add bulk d dict

--- R/data_dictionary.r
+++ R/data_dictionary.r
@@ -1,32 +1,13 @@
 
-# name:data_dictionary
-data_dictionary <- function(dataframe, variable, show_levels = -1)
-{
-  summa  <- summary(dataframe[,variable])
-  summa  <- as.data.frame(cbind(
-      c(variable, rep("", length(summa) - 1)),
-      names(summa), summa))
-  summa[,1]  <- as.character(summa[,1])
-  summa[,2]  <- as.character(summa[,2])
-  if(is.numeric(dataframe[,variable])){
-  summa$type <- c("number", rep("", nrow(summa) - 1))
-  summa$cnt <- NA
-  summa$pct  <- NA
-  } else {
-  summa$type <- c("character", rep("", nrow(summa) - 1))
-  summa$summa  <- as.numeric(as.character(summa$summa))  
-  summa$summa2 <- rep("", nrow(summa))
-      # as.numeric(as.character(summa$V2)) ?
-  summa$pct  <- round((summa$summa / sum(summa$summa)) * 100, 2)
-  summa <- summa[,c(1,2,3,5,4,6)]
-  if(show_levels > 0){
-  if(nrow(summa) > show_levels){
-      summa <- summa[1:show_levels,]  
-  summa <- rbind(summa, c("", "", sprintf("more than %s levels. list truncated.", show_levels), "","", ""))
+################################################################
+data_dictionary <- function(dataframe, show_levels = -1){
+  out <- matrix(NA, nrow = 0, ncol = 3)
+  for(i in 1:ncol(dataframe)){
+  out2 <- data_dict(.dataframe = dataframe,
+                    names(dataframe)[i],
+                    .show_levels = show_levels)
+  out <- rbind(out, out2)
   }
-  }
-  }
-  names(summa)  <- c("Variable","Type","Attributes", "Value", "Count", "Percent")
-#summa
-  return(summa)
+  row.names(out) <- NULL
+  return(out)
 }
