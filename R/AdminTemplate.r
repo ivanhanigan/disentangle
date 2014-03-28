3b284f054fe333935860637410c9a05c7bb9706c
ivanhanigan <ivanhanigan@gmail.com>
2014-03-29 10:51:55 +1100

    add admintemplate

---
+++ R/AdminTemplate.r
@@ -0,0 +1,14 @@
+
+################################################################
+# name:R-AdminTemplate
+AdminTemplate <- function(rootdir = getwd()){
+  setwd(rootdir)
+  dir.create(file.path(rootdir,'01_planning'))
+  dir.create(file.path(rootdir,'01_planning','proposal'))
+  dir.create(file.path(rootdir,'01_planning','scheduling'))
+  dir.create(file.path(rootdir,'02_budget'))
+  dir.create(file.path(rootdir,'03_communication'))
+  dir.create(file.path(rootdir,'04_reporting_and_meetings'))
+  file.create(file.path(rootdir,'contact_details.txt'))
+  file.create(file.path(rootdir,'README.md'))
+  }
---
+++ tests/test-R-AdminTemplate.r
@@ -0,0 +1,5 @@
+
+################################################################
+# name:R-AdminTemplate
+setwd("~/Dropbox/Z_New_T_Drive/Resource Management/IT Infrastructure/PhenocamsAndImageryServer")
+AdminTemplate()
