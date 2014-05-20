25cbcd3846036e9f1cdb071dbd1544cb8a59d934
Ivan <ivan.hanigan@gmail.com>
2014-05-20 14:18:20 +1000

    bb to master

--- R/morpho_bounding_box.r
+++ R/morpho_bounding_box.r
@@ -1,4 +1,3 @@
-
 ################################################################
 # name:bounding_box
 morpho_bounding_box <- function(x){
@@ -6,18 +5,18 @@ morpho_bounding_box <- function(x){
   #str(x)
   bb <- x@bbox
   #bb
-  # TODO polygons   
+  # TODO polygons
   # TODO only for southern hemisphere, east of GMT?
   loc <- data.frame(
     rbind(
-      c(NA, abs(bb[2,2]), NA),
-      c(bb[1,1],  NA, bb[1,2]),
-      c(NA, abs(bb[2,1]), NA)
+      c(NA, round(abs(bb[2,2]), 5), NA),
+      c(round(bb[1,1], 5),  NA, round(bb[1,2],5)),
+      c(NA, round(abs(bb[2,1]),5), NA)
       )
     )
   #loc
   loc$X2[c(1,3)] <- sprintf("%s S", abs(loc$X2[c(1,3)]))
   loc[2,c(1,3)] <- sprintf("%s E", abs(loc[2,c(1,3)]))
-   
+
   return(loc)
   }
