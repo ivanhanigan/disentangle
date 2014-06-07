007277ce98bc63f7937ddba3c3d7955f224b1b85
ivanhanigan <ivanhanigan@gmail.com>
2014-06-07 22:39:57 +1000

    add new code to fix structure of summa out put

--- R/data_dict.r
+++ R/data_dict.r
@@ -17,19 +17,42 @@ data_dict <- function(.dataframe, .variable, .show_levels = -1)
   summa[,1]  <- as.character(summa[,1])
   summa[,2]  <- as.character(summa[,2])
   # summa
+
+
+
+  # if char (factor)
+  if(is.factor(.dataframe[,.variable])){
+  summa$type <- c("character", rep("", nrow(summa) - 1))
+  summa$summa  <- as.numeric(as.character(summa$V3))  
+  summa$summa2 <- rep(NA, nrow(summa))
+      # as.numeric(as.character(summa$V2)) ?
+  summa$pct  <- round((summa$summa / sum(summa$summa)) * 100, 2)
+  summa <- summa[,c(1,4,2,6,5,7)]
+  if(.show_levels > 0){
+    if(nrow(summa) > .show_levels){
+      summa <- summa[1:.show_levels,]  
+      summa <- rbind(summa, c("", "",
+                              sprintf("more than %s levels. list truncated.", .show_levels),
+                              "","", "")
+                     )
+      }
+    }
+  # summa
+  } else if (
+   !all(
+      is.na(as.Date(as.character(.dataframe[,.variable]), origin = "1970-01-01"))
+      )
+    ){
   # if date
-  datevar <- as.Date(as.character(
-    .dataframe[,.variable]
-    ), origin = "1970-01-01")
+  ## datevar <- as.Date(as.character(
+  ##   .dataframe[,.variable]
+  ##   ), origin = "1970-01-01")
+  # http://stackoverflow.com/questions/18178451/is-there-a-way-to-check-if-a-column-is-a-date-in-r
   # as.Date(as.character(.dataframe[,.variable]),format="%Y-%m-%d")
   ## if(!all(is.na(datevar))){
   ##   summa[,3] <- as.character(datevar)
   ## }
-  if(
-   !all(
-      is.na(datevar)
-      )
-    ){
+    
   summa$type <- c("date", rep("", nrow(summa) - 1))
   summa$cnt <- NA
   summa$pct  <- NA
@@ -42,21 +65,7 @@ data_dict <- function(.dataframe, .variable, .show_levels = -1)
   summa$pct  <- NA
   summa <- summa[,c(1,4,2,3,5,6)]
   } else {
-  summa$type <- c("character", rep("", nrow(summa) - 1))
-  summa$summa  <- as.numeric(as.character(summa$summa))  
-  summa$summa2 <- rep(NA, nrow(summa))
-      # as.numeric(as.character(summa$V2)) ?
-  summa$pct  <- round((summa$summa / sum(summa$summa)) * 100, 2)
-  summa <- summa[,c(1,4,2,5,3,6)]
-  if(.show_levels > 0){
-    if(nrow(summa) > .show_levels){
-      summa <- summa[1:.show_levels,]  
-      summa <- rbind(summa, c("", "",
-                              sprintf("more than %s levels. list truncated.", .show_levels),
-                              "","", "")
-                     )
-      }
-    }
+    stop("type not character, factor, date or numeric")
   }
   names(summa)  <- c("Variable","Type","Attributes", "Value", "Count", "Percent")
 #summa
