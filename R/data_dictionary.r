310112e2c15fecf81c113f84989a3a14569c5969
ivanhanigan <ivanhanigan@gmail.com>
2014-06-07 22:51:29 +1000

    try again

--- R/data_dictionary.r
+++ R/data_dictionary.r
@@ -3,9 +3,15 @@
 data_dictionary <- function(dataframe, show_levels = -1){
   out <- matrix(NA, nrow = 0, ncol = 3)
   for(i in 1:ncol(dataframe)){
-  out2 <- data_dict(.dataframe = dataframe,
-                    names(dataframe)[i],
-                    .show_levels = show_levels)
+  #  i = 1
+    print(i)
+  out2 <- data_dict(
+    .dataframe = dataframe
+    ,
+    .variable = names(dataframe)[i]
+    ,
+    .show_levels = show_levels
+    )
   out <- rbind(out, out2)
   }
   row.names(out) <- NULL
