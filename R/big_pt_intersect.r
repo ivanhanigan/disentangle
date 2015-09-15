392d5d0376c575f8a04c74e5dddc0a8c20b3ecfc
ivanhanigan <ivan.hanigan@gmail.com>
2015-09-15 15:28:52 +1000

    add big_pt_intersect, this will require pushing out another windoze version

---
+++ R/big_pt_intersect.r
@@ -0,0 +1,31 @@
+
+#' @title big points intersect
+#' @name big_pt_intersect
+#' @param pts a points spatial
+#' @param ply a polygons spatial
+#' @param chunks how many to split into
+#' @return a combined data frame that avoids run out of memory with big points file 
+big_pt_intersect <- function(pts, ply, chunks = 100){
+  idx <- split(pts@data, 1:chunks)
+  #str(idx)
+  for(i in 1:length(idx)){
+  #i = 1
+  print(i)
+    ids <- idx[[i]][,1]
+  #str(pts@data)
+  qc <- pts[pts@data[,1] %in% ids,]
+  #str(qc)
+  tryCatch(chunk <-  intersect(qc, ply), error = function(err){print(err)})
+  if(exists('chunk')){
+  if(i == 1){
+    chunk_out <- chunk@data
+  } else {
+    chunk_out <- rbind(chunk_out, chunk@data)
+  }
+  rm(chunk)
+  }
+  }
+  #str(chunk_out)
+  return(chunk_out)
+}
+# NB warning about split length multiple is not fatal, just due to nonequal chunks (ie the geocodes are 2009/100)
