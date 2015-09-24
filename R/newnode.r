cb0b3537f164d24a13f4917cc192f3eacd326f2b
ivanhanigan <ivan.hanigan@gmail.com>
2015-09-24 11:58:30 +1000

    Change newnode to add NA to list of inputs for colour of status

--- R/newnode.r
+++ R/newnode.r
@@ -66,7 +66,7 @@ strng <- sprintf('%s\n%s  [ shape=record, label="{{ { Name | Description | Statu
                  inputs_listed, name2paste, name, desc, status, outputs_listed
                  )
       # cat(strng)
-      if(!status %in% c("DONE", "WONTDO")){ 
+      if(!status %in% c("DONE", "WONTDO", "")){ 
         strng <- gsub("shape=record,", "shape=record, style = \"filled\", color=\"indianred\",", strng)
       }
     } else {
