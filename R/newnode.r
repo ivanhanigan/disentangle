5f67bd2cf7f80e1ee7d3bd9cd7896731263e638f
ivanhanigan <ivan.hanigan@gmail.com>
2015-11-22 23:49:11 +1100

    add changes to test colours within nodes

--- R/newnode.r
+++ R/newnode.r
@@ -9,7 +9,7 @@
 #' @param clusters_col optional column identifying clusters
 #' @param todo_col optional column with TODO status (DONE and WONTDO will be white, others are red)
 #' @return character string object that has the DOT language representatio of the input
-newnode <- function(indat, names_col = NULL, in_col = NULL, out_col = NULL, desc_col = NULL, clusters_col = NULL, todo_col = NULL, nchar_to_snip = 40){
+newnode <- function(indat, names_col = NULL, in_col = NULL, out_col = NULL, desc_col = NULL, clusters_col = NULL, todo_col = NULL, nchar_to_snip = 40, colour_col = NULL){
 if (is.null(names_col)) stop("Names of the steps are needed")
 if (is.null(in_col)) stop("Inputs are needed")
 if (is.null(out_col)) stop("Outputs are needed")
@@ -21,7 +21,7 @@ if (is.null(desc_col)){
 
 # sanitize any single quotes
 for(i in 1:ncol(indat)){
- indat[,i] <- gsub("'","", indat[,i]) 
+ indat[,i] <- gsub("'","", indat[,i])
 }
 
 if(!is.null(clusters_col)){
@@ -38,13 +38,13 @@ for(cluster_i in cluster_ids){
   } else {
     nodes_graph <- paste(nodes_graph, sprintf('subgraph cluster_%s {
     label = "%s"
-    ', cluster_i, cluster_i))  
+    ', cluster_i, cluster_i))
   }
-#  cat(nodes_graph)    
+#  cat(nodes_graph)
   indat2 <- indat[indat[,clusters_col] == cluster_i,]
-#  indat2
+# indat2
     for(i in 1:nrow(indat2)){
-      # i <- 1
+      #i <- 2
       #i
       indat2[i,]
       name <- indat2[i,names_col]
@@ -60,30 +60,39 @@ for(cluster_i in cluster_ids){
       #inputs
       inputs_listed <- paste(inputs, name2paste, sep = ' -> ', collapse = "\n")
       #cat(inputs_listed)
-      outputs <- paste('"', outputs, '"', sep = "")  
+      outputs <- paste('"', outputs, '"', sep = "")
       outputs_listed <- paste(name2paste, outputs, sep = ' -> ', collapse = "\n")
       #cat(outputs_listed)
+
 if(!is.null(todo_col)){
-      
-      status <- indat2[i,todo_col]      
-      
-strng <- sprintf('%s\n%s  [ shape=record, label="{{ { Name | Description | Status } | { %s | %s | %s } }}"]\n%s\n\n',
+
+      status <- indat2[i,todo_col]
+
+strng <- sprintf('%s\n%s  [ shape=record, style = \"filled\", fillcolor=\"white\", label="{{ { Name | Description | Status } | { %s | %s | %s } }}"]\n%s\n\n',
                  inputs_listed, name2paste, name, desc, status, outputs_listed
                  )
       # cat(strng)
-      if(!status %in% c("DONE", "WONTDO", "", NA)){ 
-        strng <- gsub("shape=record,", "shape=record, style = \"filled\", color=\"indianred\",", strng)
+      if(!status %in% c("DONE", "WONTDO", "", NA)){
+        strng <- gsub("shape=record,", "shape=record, style = \"filled\", fillcolor=\"indianred\",", strng)
       }
     } else {
-  
-strng <- sprintf('%s\n%s  [ shape=record, label="{{ { Name | Description } | { %s | %s } }}"]\n%s\n\n',
+
+strng <- sprintf('%s\n%s  [ shape=record, style = \"filled\", fillcolor=\"white\", label="{{ { Name | Description } | { %s | %s } }}"]\n%s\n\n',
                  inputs_listed, name2paste, name, desc, outputs_listed
                  )
 }
       nodes_graph <- paste(nodes_graph, strng, "\n")
       if(nrow(indat2) == 1) break
     }
-nodes_graph <- paste(nodes_graph, "}\n\n")
+
+# cat(nodes_graph)
+# set col
+if(!is.na(indat2[i,colour_col])){
+ nodes_graph <- gsub("white", indat2[i,colour_col], nodes_graph)
+}
+#cat(nodes_graph)
+###########
+  nodes_graph <- paste(nodes_graph, "}\n\n")
 }
 } else {
   indat2 <- indat
@@ -105,28 +114,30 @@ nodes_graph <- paste(nodes_graph, "}\n\n")
       #inputs
       inputs_listed <- paste(inputs, name2paste, sep = ' -> ', collapse = "\n")
       #cat(inputs_listed)
-      outputs <- paste('"', outputs, '"', sep = "")  
+      outputs <- paste('"', outputs, '"', sep = "")
       outputs_listed <- paste(name2paste, outputs, sep = ' -> ', collapse = "\n")
       #cat(outputs_listed)
 if(!is.null(todo_col)){
   status <- indat2[i,todo_col]
-       
+
 strng <- sprintf('%s\n%s  [ shape=record, label="{{ { Name | Description | Status } | { %s | %s | %s } }}"]\n%s\n\n',
                  inputs_listed, name2paste, name, desc, status, outputs_listed
                  )
       # cat(strng)
-      if(!status %in% c("DONE", "WONTDO", "", NA)){ 
+      if(!status %in% c("DONE", "WONTDO", "", NA)){
         strng <- gsub("shape=record,", "shape=record, style = \"filled\", color=\"indianred\",", strng)
       }
 } else {
-  
-strng <- sprintf('%s\n%s  [ shape=record, label="{{ { Name | Description } | { %s | %s } }}"]\n%s\n\n',
+
+strng <- sprintf('%s\n%s  [ shape=record, style = \"filled\", fillcolor=\"white\", label="{{ { Name | Description } | { %s | %s } }}"]\n%s\n\n',
                  inputs_listed, name2paste, name, desc, outputs_listed
                  )
 }
       nodes_graph <- paste(nodes_graph, strng, "\n")
 }
 }
+
+
 nodes_graph <- paste("digraph transformations {\n\n",
                      nodes_graph,
                      "}\n")
@@ -145,3 +156,4 @@ DiagrammeR::grViz("file_name.dot")
 
 return(nodes_graph)
 }
+cat(nodes_graph)
