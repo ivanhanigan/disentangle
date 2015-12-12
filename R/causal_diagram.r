6dd531b9ed289e036775a2b5fe30908145d4f0fd
ivanhanigan <ivan.hanigan@gmail.com>
2015-12-13 08:18:36 +1100

    add causal_diagram function

---
+++ R/causal_diagram.r
@@ -0,0 +1,46 @@
+
+causal_diagram <- function(
+indat2  = nodes
+
+in_col = "causes"
+
+out_col  = "effect"
+
+colour_col = "colour"
+
+pos_col = "pos"
+){
+nodes2 <-as.data.frame(matrix(ncol = 2, nrow = 0))
+names(nodes2)  <- c("inputs", "outputs")
+nameslist <- character(0)
+colourslist <- character(0)
+poslist <- character(0)
+for(i in 1:nrow(indat2)){
+#  i <- 1
+  #i
+  indat2[i,]
+  inputs <- unlist(lapply(strsplit(indat2[i,in_col], ","), str_trim))
+  outputs <- unlist(lapply(strsplit(indat2[i,out_col], ","), str_trim))
+  if(length(inputs) > 0){
+    nodes2 <- rbind(nodes2, cbind(inputs, outputs))
+  }
+  nameslist <- c(nameslist, outputs)
+  colourslist <- c(colourslist, indat2[i, colour_col]) 
+  poslist <- c(poslist, indat2[i, pos_col])
+}
+## nodes2
+## nameslist
+## colourslist
+## poslist
+edges_outcome <- create_edges(from = nodes2$inputs,
+                        to =   nodes2$outputs
+                        )
+
+nodes_outcome <- create_nodes(nodes = nameslist,
+                        label = TRUE,
+                        color = colourslist, pos = poslist)
+#nodes_outcome
+graph_outcome <- create_graph(nodes_df = nodes_outcome,
+                       edges_df = edges_outcome)
+return(graph_outcome)
+}
---
+++ tests/test-causal_diagram.r
@@ -0,0 +1,44 @@
+
+# name:causal_diagram
+library(DiagrammeR)
+library(stringr)
+nodes  <- read.csv(textConnection('causes,         effect, colour, pos
+                , Rainfall deficit,                           , "-1,3!"
+Rainfall deficit, Drought,                                 indianred, "-0.5,2!"
+               , Insular society, ,                             "1.5,3!"
+Insular society, Anomie, ,                                      "1,2!"
+"Drought, Anomie", Altered social structures and dynamics, gray,  "0,0!"
+Altered social structures and dynamics, Depression, lightblue,         "4,0!"
+Depression,                             Suicide   , lightgreen,        "7,0!"
+'), stringsAsFactors = F, strip.white = T)
+nodes
+
+dotty <- causal_diagram(
+  indat2  = nodes
+  ,
+  in_col = "causes"
+  ,
+  out_col  = "effect"
+  ,
+  colour_col = "colour"
+  ,
+  pos_col = "pos"
+  )
+#cat(dotty$dot_code)
+
+# just test this out
+#render_graph(dotty)
+#render_graph(dotty, output = "visNetwork")
+
+# actual control
+dotty2 <- gsub("digraph \\{",
+"digraph \\{
+graph [layout = neato]
+node [fontname = Helvetica,
+     style = filled]
+edge [color = gray20,
+     arrowsize = 1,
+     fontname = Helvetica]",
+dotty$dot_code)
+#cat(dotty2)
+grViz(dotty2)
